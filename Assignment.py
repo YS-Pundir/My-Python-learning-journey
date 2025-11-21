1de7b8036a00533a464f859dd83d20cb05330470
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-21 22:31:32 +0100

    adding function to create a list

---
+++ Assignment.py
@@ -0,0 +1,14 @@
+list=[]
+length=len(list)
+length=int(input("enter the length of the list: ",))
+
+def create_list():#function to create a list
+    if length>=2 and length<=10:#as requirement : length should be bitween 2 and 10
+        for i in range(length):
+            el=int(input("enter the element :",))
+            list.append(el)
+    else:
+        print("length should be bitween 2 and 10 ")
+
+create_list()
+print("the created list is :",list)
