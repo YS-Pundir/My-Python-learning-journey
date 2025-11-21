c0327c767447486cc5639e806b5c841413bbcf7b
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-21 22:36:03 +0100

    chaging variables according to the requirements

--- Assignment.py
+++ Assignment.py
@@ -1,14 +1,16 @@
-list=[]
-length=len(list)
+numbers=[]
+length=len(numbers)
 length=int(input("enter the length of the list: ",))
 
 def create_list():#function to create a list
     if length>=2 and length<=10:#as requirement : length should be bitween 2 and 10
         for i in range(length):
             el=int(input("enter the element :",))
-            list.append(el)
+            numbers.append(el)
     else:
         print("length should be bitween 2 and 10 ")
 
 create_list()
-print("the created list is :",list)
+print("the created list is :",numbers)
+
+#function to find the 
