a60b7b89eb1dadd4585fa66593095c469e402748
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-21 22:55:35 +0100

    modifing some mistakes

--- Assignment.py
+++ Assignment.py
@@ -1,6 +1,5 @@
-numbers=[]
-length=len(numbers)
-length=int(input("enter the length of the list: ",))
+numbers = []
+length = int(input("enter the length of the list: "))
 
 def create_list():#function to create a list
     if length>=2 and length<=10:#as requirement : length should be bitween 2 and 10
@@ -14,38 +13,39 @@ create_list()
 print("the created list is :",numbers)
 
 #function to find the sum of all elements in the list
-def compute_sum(numbers):
-    sum=0
-    for i in range (numbers):
-        sum+=i
-    return sum
+def compute_sum(nums):
+    total = 0
+    for i in nums:
+        total += i
+    return total
 
 #function to find the average of all elements in the list
-def compute_average(numbers):
-    avg=compute_sum(numbers)/length
-    return avg
+def compute_average(nums):
+    if len(nums) == 0:
+        return 0
+    return compute_sum(nums) / len(nums)
 
 #function to find the maximum element in the list using for loop
-def find_largest(numbers):
-    max=numbers[0]
-    for i in numbers:
-        if i>max:
-            max=i
-    return max
+def find_largest(nums):
+    largest = nums[0]
+    for i in nums:
+        if i > largest:
+            largest = i
+    return largest
 
 #function to find minimum element in the list using for loop
-def find_smallest(numbers):
-    min=numbers[0]
-    for i in numbers:
-        if i<min:
-            min=i
-    return min
+def find_smallest(nums):
+    smallest = nums[0]
+    for i in nums:
+        if i < smallest:
+            smallest = i
+    return smallest
 
 #function to display the results of all peroformed operations
-def display_funciton():
-    print("the sum of all elements in the list is :",compute_sum(length))
-    print("the average of all elements in the list is :",compute_average(numbers))
-    print("the largest element in the list is :",find_largest(numbers))
-    print("the smallest element in the list is :",find_smallest(numbers))
-    
-display_funciton()
+def display_function():
+    print("the sum of all elements in the list is :", compute_sum(numbers))
+    print("the average of all elements in the list is :", compute_average(numbers))
+    print("the largest element in the list is :", find_largest(numbers))
+    print("the smallest element in the list is :", find_smallest(numbers))
+
+display_function()
