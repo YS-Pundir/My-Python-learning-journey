70a5e9a7db8f7f0333e4ab5ff33937760880c47d
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-22 12:16:05 +0100

    outside the class , adding progarme to take input from user

--- Assignment2/Code.py
+++ Assignment2/Code.py
@@ -10,6 +10,13 @@ class NumberProcessor:
         for i in self.List:
             sum+=i
         return sum
+#taking input from user
+list1=[]
+for i in range(5):
+    n=int(input("enter the number : ",))
+    list1.append(n)
+list1=NumberProcessor(list1)
+print("the sum of all elements in the list is : ",list1.calculate_sum())
 
 
     
