ed8076b73e06cf9d85a95d8157d979daedc2b6b0
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-11-22 12:23:11 +0100

    addding method to find the largest element in the list

--- Assignment2/Code.py
+++ Assignment2/Code.py
@@ -13,6 +13,13 @@ class NumberProcessor:
     #method to caalculate average of all elements in the list
     def calculate_average(self):
         return self.calculate_sum()/len(self.List)
+    #method to find largest element in the list
+    def find_largest(self):
+        max=self.List[0]
+        for i in self.List:
+            if i>max:
+                max=i
+        return max
         
 
 #taking input form user
@@ -23,6 +30,7 @@ for i in range(5):
 list1=NumberProcessor(list1)
 print("the sum of all elements in the list is : ",list1.calculate_sum())
 print("the average of all elements in the list is : ",list1.calculate_average())
+print("the largest element in the list is : ",list1.find_largest())
 
 
     
